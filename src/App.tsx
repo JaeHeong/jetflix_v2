f0985a42b1968a250ac561cc8eb77c782026832f
JaeHeong <rlawogud970301@gmail.com>
2023-03-29 22:27:00 +0900

    make base for jetflix

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,17 @@ function App() {
     <Router>
       <Header />
       <Switch>
-        <Route path="/tv">
-          <Tv />
-        </Route>
-        <Route path="/search">
-          <Search />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
+        <>
+          <Route path="/tv">
+            <Tv />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path={["/", "/movies/:movieId"]}>
+            <Home />
+          </Route>
+        </>
       </Switch>
     </Router>
   );
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,15 +1,21 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { Link, useRouteMatch } from "react-router-dom";
+import {
+  motion,
+  useAnimation,
+  useMotionValueEvent,
+  useScroll,
+} from "framer-motion";
+import { Link, useHistory, useRouteMatch } from "react-router-dom";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-const Nav = styled.nav`
+const Nav = styled(motion.nav)`
   display: flex;
   justify-content: space-between;
   align-items: center;
   position: fixed;
   width: 100%;
   top: 0;
-  background-color: black;
   font-size: 14px;
   padding: 20px 60px;
   color: white;
@@ -44,22 +50,17 @@ const Item = styled.li`
   }
 `;
 
-const Search = styled.span`
+const Search = styled.form`
   color: white;
+  display: flex;
+  align-items: center;
+  position: relative;
   svg {
     height: 25px;
   }
 `;
 
-const logoVariants = {
-  animate: {
-    rotate: [0, -5, 10, -8, 0],
-    y: [0, -8, 0],
-    scale: [1, 0.95, 1.1, 1],
-  },
-};
-
-const Circle = styled.span`
+const Circle = styled(motion.span)`
   position: absolute;
   width: 5px;
   height: 5px;
@@ -71,11 +72,77 @@ const Circle = styled.span`
   background-color: ${(props) => props.theme.red};
 `;
 
+const Input = styled(motion.input)`
+  transform-origin: right center;
+  position: absolute;
+  right: 0px;
+  padding: 5px 10px;
+  padding-left: 40px;
+  z-index: -1;
+  color: white;
+  font-size: 16px;
+  background-color: transparent;
+  border: 1px solid ${(props) => props.theme.white.lighter};
+`;
+
+const logoVariants = {
+  animate: {
+    opacity: [1, 0, 0.2, 1, 0, 0.2, 1],
+    rotate: [0, -5, 10, -8, 0],
+    y: [0, -8, 2, 0],
+    scale: [1, 0.95, 1.1, 1],
+  },
+};
+
+const navVariants = {
+  top: {
+    backgroundColor: "rgba(0,0,0,0)",
+  },
+  scroll: {
+    backgroundColor: "rgba(0,0,0,1)",
+  },
+};
+
+interface IForm {
+  keyword: string;
+}
+
 function Header() {
+  const [searchOpen, setSearchOpen] = useState(false);
   const homeMatch = useRouteMatch("/");
   const tvMatch = useRouteMatch("/tv");
+  const inputAnimation = useAnimation();
+  const navAnimation = useAnimation();
+  const { scrollY } = useScroll();
+  const toggleSearch = () => {
+    if (searchOpen) {
+      inputAnimation.start({
+        scaleX: 0,
+        opacity: 0,
+      });
+    } else {
+      inputAnimation.start({
+        scaleX: 1,
+        opacity: 1,
+      });
+    }
+    setSearchOpen((prev) => !prev);
+  };
+  useMotionValueEvent(scrollY, "change", (y) => {
+    if (y > 80) {
+      navAnimation.start("scroll");
+    } else {
+      navAnimation.start("top");
+    }
+  });
+  const history = useHistory();
+  const { register, handleSubmit } = useForm<IForm>();
+  const onValid = (data: IForm) => {
+    history.push(`/search?keyword=${data.keyword}`);
+  };
+
   return (
-    <Nav>
+    <Nav variants={navVariants} animate={navAnimation} initial={"top"}>
       <Col>
         <Logo
           variants={logoVariants}
@@ -84,16 +151,23 @@ function Header() {
         ></Logo>
         <Items>
           <Item>
-            <Link to="/">Home {homeMatch?.isExact && <Circle />}</Link>
+            <Link to="/">
+              Home {homeMatch?.isExact && <Circle layoutId="circle" />}
+            </Link>
           </Item>
           <Item>
-            <Link to="/tv">Tv Shows {tvMatch && <Circle />}</Link>
+            <Link to="/tv">
+              Tv Shows {tvMatch && <Circle layoutId="circle" />}
+            </Link>
           </Item>
         </Items>
       </Col>
       <Col>
-        <Search>
-          <svg
+        <Search onSubmit={handleSubmit(onValid)}>
+          <motion.svg
+            onClick={toggleSearch}
+            animate={{ x: searchOpen ? -215 : 0 }}
+            transition={{ type: "linear" }}
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -103,7 +177,14 @@ function Header() {
               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
               clipRule="evenodd"
             ></path>
-          </svg>
+          </motion.svg>
+          <Input
+            {...register("keyword", { required: true, minLength: 2 })}
+            animate={inputAnimation}
+            initial={{ scaleX: 0 }}
+            transition={{ type: "linear" }}
+            placeholder="Search for movie or tv show..."
+          />
         </Search>
       </Col>
     </Nav>
