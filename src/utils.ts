33de419e3ba24f7618ee38f6386ebf9136683a98
jaehyeong <rlawogud970301@gmail.com>
2023-07-16 01:41:09 +0900

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,13 @@
-const BASE_PATH = "http://192.168.163.20:8080";
+const BASE_PATH = "https://d1bug73j39exox.cloudfront.net";
 
 export function makeBgPath(id: number) {
-  return `${BASE_PATH}/images/bg/${id}`;
+  return `${BASE_PATH}/${id}/bg.jpeg`;
 }
 
 export function makePosterPath(id: number) {
-  return `${BASE_PATH}/images/poster/${id}`;
+  return `${BASE_PATH}/${id}/post.jpeg`;
 }
 
 export function makePlayPath(id: number) {
-  return `${BASE_PATH}/videos/play/${id}`;
+  return `${BASE_PATH}/${id}/pv.mp4`;
 }
